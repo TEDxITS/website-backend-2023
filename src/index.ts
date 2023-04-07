bd13750a0ae2170badaab36c639cc17e818ba2e5
christhopermarcelino <christhopermarcelino@gmail.com>
2023-04-08 00:12:50 +0700

    feat: add refresh token api

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,19 @@ import {
 import env from "./config/LoadEnv";
 import { sendOk } from "./helper/ApiResponse";
 
+import AuthRouter from "./router/AuthRouter";
+
 const app: Express = express()
 const PORT = env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_: Request, res: Response) =>  {
+app.get("/api", async (_: Request, res: Response) =>  {
     sendOk(res, StatusCodes.OK, "TEDxITS 2023 API")
 })
+
+app.use("/api/auth", AuthRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
