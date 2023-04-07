c0eff6feeebef64692bd7b97e52288b081460ac2
Mikask <darrenprasetya40@gmail.com>
2023-04-07 22:18:09 +0700

    feat: add partially done register and verification

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,12 +1,76 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
 
-import { sendData, sendError } from "../helper/ApiResponse";
+import { sendData, sendError, sendOk } from "../helper/ApiResponse";
 import * as AuthService from "../service/AuthService";
 import { AdminTokenData, UserTokenData } from "../middleware/AuthMiddleware";
 import env from "../config/LoadEnv";
 import { CustomError } from "../helper/Error";
+import db from "../config/Db";
+// import { GenerateRandomToken } from "./../util/Util";
+
+type RegisterBody = {
+	name: string;
+	email: string;
+	password: string;
+};
+
+export const Register = async (
+	req: Request<{}, {}, RegisterBody>,
+	res: Response
+) => {
+	const data = req.body;
+
+	if (!data.email || !data.name || !data.password) {
+        sendError(res, new CustomError(StatusCodes.BAD_REQUEST,"Insufficient data to register user"))
+		return;
+	}
+
+	var hashedPassword;
+
+	try {
+		hashedPassword = await bcrypt.hash(data.password, 12);
+	} catch (error) {
+        sendError(res, error, null)
+		return;
+	}
+
+	try {
+		await db.user.create({
+			data: { ...req.body, password: hashedPassword, isVerified: false },
+		});
+	} catch (error) {
+        sendError(res, error, null)
+		return;
+	}
+
+	// const verifyToken = GenerateRandomToken();
+
+	// TODO: send email
+
+	sendOk(res, 201, "Successfully registered user");
+};
+
+type VerifyAccountParams = {
+	verifyToken: string;
+};
+
+export const VerifyAccount = async (
+	req: Request<VerifyAccountParams>,
+	res: Response
+) => {
+	// const { verifyToken } = req.params;
+
+	// await db.accountVerification.findUniqueOrThrow({ where: { } });
+
+    // TODO: account verification
+
+    // await db.user.update({data: {isVerified: true}, where: {}})
+
+	sendOk(res, 200, "Successfully verified account");
+};
 
 export const refreshAdminToken = (req: Request, res: Response) => {
     const bearerHeader = req.headers.authorization
