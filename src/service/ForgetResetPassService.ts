1883423aa55ba94ad0712b007f7bfc3dc215fff4
balakatumtum <fawwazrazani@gmail.com>
2023-04-11 22:20:31 +0700

    feat: migrate db related code to repository

--- src/service/ForgetResetPassService.ts
+++ src/service/ForgetResetPassService.ts
@@ -1,63 +1,39 @@
 import { StatusCodes } from "http-status-codes";
-import db from "../config/Db";
 import { CustomError } from "../helper/Error";
-import { ResetPasswordRequest } from "../model/ForgetResetPassModel";
-import { getUserByEmail } from "../repository/UserRepository";
+import { ForgetPasswordRequest } from "../model/ForgetResetPassModel";
+import { getUserByEmail, updateUserPassword } from "../repository/UserRepository";
 import { sendEmail } from "./email";
 import { GenerateRandomToken } from "../util/Util";
+import { createResetToken, deleteResetToken, getResetTokenbyToken } from "../repository/ForgetPasswordRepository";
 
-const bcrypt = require("bcryptjs");
-
-export const sendForgetPasswordEmail = async (data: ResetPasswordRequest) => {
+export const sendForgetPasswordEmail = async (data: ForgetPasswordRequest) => {
     const user = await getUserByEmail(data.email)
     
     if(!user) {
         throw new CustomError(StatusCodes.NOT_FOUND, "Email is not Registered")
     }
 
-    const ResetPassToken = GenerateRandomToken();
+    const resetPassToken = await GenerateRandomToken();
 
-    await db.forgetPassword.create({
-        data: {
-            userId: user.id,
-            token: ResetPassToken
-        }
-    })
-
-    await sendEmail({
-        subject: "Your TEDxITS Account Recovery",
-        to: data.email,
-        html: 'Click <a href="https://www.tedxits.org/reset-password//${ResetPassToken}" target="_blank">here</a> to recover your account.'
-    });
+    await createResetToken(user.id, resetPassToken).then(() =>
+        sendEmail({
+            subject: "Your TEDxITS Account Recovery",
+            to: data.email,
+            html: 'Click <a href="https://www.tedxits.org/reset-password/${resetPassToken}" target="_blank">here</a> to recover your account.'
+        })
+    );
 
     return;
 }
 
-export const resetPassword = async (resetPassToken: string, newPassword: string) => {
-    const Data = await db.forgetPassword.findUnique({
-        where: {
-            token: resetPassToken
-        }
-    })
+export const resetPassword = async (resetPassToken: string, newPassword: any) => {
+    const data = await getResetTokenbyToken(resetPassToken);
 
-    if(!Data){
+    if(!data){
         throw new CustomError(StatusCodes.NOT_FOUND, "Token is Invalid");
     }
 
-    await db.user.update({
-        where: {
-            id: Data.userId
-        },
-        data: {
-            password: await bcrypt.hash(newPassword, 12)
-        }
-    })
+    await updateUserPassword(data.id, newPassword);
 
-    await db.forgetPassword.delete({
-        where: {
-            token: resetPassToken
-        }
-    })
-
-    return;
+    await deleteResetToken(resetPassToken);
 }
