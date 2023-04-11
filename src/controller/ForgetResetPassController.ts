eac4f4c42990681161ac412eb2d818b616f3defe
balakatumtum <fawwazrazani@gmail.com>
2023-04-11 22:17:48 +0700

    feat: add joi validation for password

--- src/controller/ForgetResetPassController.ts
+++ src/controller/ForgetResetPassController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
 import { sendError, sendOk } from "../helper/ApiResponse";
 import { StatusCodes } from "http-status-codes";
-import { forgetPasswordSchema } from "../helper/Validation";
-import { ResetPasswordRequest } from "../model/ForgetResetPassModel";
+import { forgetPasswordSchema, resetPasswordSchema } from "../helper/Validation";
+import { ForgetPasswordRequest, ResetPasswordRequest } from "../model/ForgetResetPassModel";
 import { CustomError } from "../helper/Error";
 import Joi from 'joi';
 import { resetPassword, sendForgetPasswordEmail } from "../service/ForgetResetPassService";
 
 export const forgetPassword = async (req: Request, res: Response) => {
-    const { error, value }: {error: Joi.ValidationError | undefined, value: ResetPasswordRequest | undefined} = forgetPasswordSchema.validate(req.body, { abortEarly: false });
+    const { error, value }: {error: Joi.ValidationError | undefined, value: ForgetPasswordRequest | undefined} = forgetPasswordSchema.validate(req.body, { abortEarly: false });
 
     if (error) {
 		return sendError(res, new CustomError(StatusCodes.BAD_REQUEST, "Wrong Format"), error);
 	}
     
     try{
-        await sendForgetPasswordEmail(value as ResetPasswordRequest);
+        await sendForgetPasswordEmail(value as ForgetPasswordRequest);
 
         return sendOk(res, StatusCodes.OK, "Email Sent Successfully");
     } catch(err){
@@ -28,14 +28,19 @@ type ResetPassTokenParams = {
 };
 
 export const resetUserPassword = async (req: Request<ResetPassTokenParams>, res: Response) => {
+    const { error, newPassword }: {error: Joi.ValidationError | undefined, newPassword: ResetPasswordRequest | undefined} = await resetPasswordSchema.validate(req.body.password, { abortEarly: false });
+
+    if (error) {
+		return sendError(res, new CustomError(StatusCodes.BAD_REQUEST, "Wrong Format"), error);
+	}
+    
     const { resetPassToken } = req.params;
 
     if(!resetPassToken){
         return sendError(res, new CustomError(StatusCodes.BAD_REQUEST, "Token is Required") );
     }
 
-    try{
-        const newPassword = req.body.password;
+    try{   
         await resetPassword(resetPassToken, newPassword);
 
         return sendOk(res,StatusCodes.OK, "Password Changed Successfully")
