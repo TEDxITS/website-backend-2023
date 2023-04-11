b1c1c3fabcfd2f676935a73ee01ad8793ebb9f72
balakatumtum <fawwazrazani@gmail.com>
2023-04-11 22:19:36 +0700

    feat: create repo between db and forgetpassword

---
+++ src/repository/ForgetPasswordRepository.ts
@@ -0,0 +1,38 @@
+import db from "../config/Db";
+
+export const getResetTokenbyToken =async (token: string) => {
+    try {
+        return await db.forgetPassword.findUnique({
+            where: {
+                token: token
+            }
+        })
+    } catch(err){
+        throw err;
+    }
+}
+
+export const createResetToken = async (userId: string, token: string) => {
+    try {
+        await db.forgetPassword.create({
+            data: {
+                userId: userId,
+                token: token
+            }
+        })
+    } catch(err){
+        throw err;
+    }
+}
+
+export const deleteResetToken = async (token: string) => {
+    try {
+        await db.forgetPassword.delete({
+            where: {
+                token: token
+            }
+        })
+    } catch(err) {
+        
+    }
+}
