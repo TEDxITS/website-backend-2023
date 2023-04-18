a2bdcae148809ccb81b92827c2796364dc43a8cb
christhopermarcelino <christhopermarcelino@gmail.com>
2023-04-19 00:06:14 +0700

    feat: change ticket seed data

--- src/seed/TicketSeed.ts
+++ src/seed/TicketSeed.ts
@@ -4,55 +4,55 @@ export const tickets = [
     {
         id: env.ID_TICKET_EARLY_BIRD_WITH_KIT,
         name: "Early Bird",
-        quota: 20,
-        price: 80000,
+        quota: 15,
+        price: 115000,
         type: "With Kit",
-        dateOpen: new Date("2023-04-14 16:00:00"),
-        dateClose: new Date("2023-04-15 23:59:00")
+        dateOpen: new Date("2023-04-20 00:00:00"),
+        dateClose: new Date("2023-04-21 23:59:59")
     },
     {
         id: env.ID_TICKET_PRE_SALE_WITH_KIT,
-        name: "Early Bird",
-        quota: 20,
-        price: 80000,
+        name: "Pre Sale",
+        quota: 15,
+        price: 115000,
         type: "With Kit",
-        dateOpen: new Date("2023-04-14 16:00:00"),
-        dateClose: new Date("2023-04-15 23:59:00")
+        dateOpen: new Date("2023-04-24 00:00:00"),
+        dateClose: new Date("2023-04-27 23:59:59")
     },
     {
         id: env.ID_TICKET_NORMAL_WITH_KIT,
         name: "Normal",
-        quota: 55,
-        price: 110000,
+        quota: 25,
+        price: 135000,
         type: "With Kit",
-        dateOpen: new Date("2023-05-02T16:00:00"),
-        dateClose: new Date("2023-05-16T23:59:00")
+        dateOpen: new Date("2023-05-02 00:00:00"),
+        dateClose: new Date("2023-05-16 23:59:59")
     },
     {
         id: env.ID_TICKET_EARLY_BIRD_NON_KIT,
         name: "Early Bird",
-        quota: 10,
-        price: 105000,
+        quota: 30,
+        price: 85000,
         type: "Non Kit",
-        dateOpen: new Date("2023-04-14 16:00:00"),
-        dateClose: new Date("2023-04-15 23:59:00")
+        dateOpen: new Date("2023-04-20 00:00:00"),
+        dateClose: new Date("2023-04-21 23:59:59")
     },
     {
         id: env.ID_TICKET_PRE_SALE_NON_KIT,
         name: "Pre Sale",
-        quota: 15,
-        price: 115000,
+        quota: 40,
+        price: 90000,
         type: "Non Kit",
-        dateOpen: new Date("2023-04-24T16:00:00"),
-        dateClose: new Date("2023-04-27T23:59:00")
+        dateOpen: new Date("2023-04-24 00:00:00"),
+        dateClose: new Date("2023-04-27 23:59:59")
     },
     {
         id: env.ID_TICKET_NORMAL_NON_KIT,
         name: "Normal",
-        quota: 25,
-        price: 135000,
+        quota: 55,
+        price: 110000,
         type: "Non Kit",
-        dateOpen: new Date("2023-05-02T16:00:00"),
-        dateClose: new Date("2023-05-16T23:59:00")
+        dateOpen: new Date("2023-05-02 00:00:00"),
+        dateClose: new Date("2023-05-16 23:59:59")
     }
 ]
