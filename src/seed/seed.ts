bf8a8ca8b211baa366506eea6882232357c558d5
christhopermarcelino <christhopermarcelino@gmail.com>
2023-04-19 00:06:46 +0700

    feat: change seed method to upsert (update or insert)

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -5,8 +5,20 @@ import { payments } from "./PaymentSeed";
 async function main(){
   try {
     tickets.forEach(async ticket => {
-      await db.ticket.create({
-        data: {
+      await db.ticket.upsert({
+        where: {
+          id: ticket.id
+        },
+        update: {
+          id: ticket.id,
+          name: ticket.name,
+          quota: ticket.quota,
+          price: ticket.price,
+          type: ticket.type,
+          dateOpen: ticket.dateOpen,
+          dateClose: ticket.dateClose
+        },
+        create: {
           id: ticket.id,
           name: ticket.name,
           quota: ticket.quota,
@@ -19,8 +31,17 @@ async function main(){
     })
 
     payments.forEach(async payment => {
-      await db.payment.create({
-        data: {
+      await db.payment.upsert({
+        where: {
+          id: payment.id
+        },
+        update: {
+          id: payment.id,
+          name: payment.name,
+          personName: payment.personName,
+          number: payment.number
+        },
+        create: {
           id: payment.id,
           name: payment.name,
           personName: payment.personName,
